ad07e41f2642617fbc953b70ac06ca2354449851
Larionov B <79999987388>
2021-11-25 16:38:58 +0300

    complite

--- core/src/ru/larionov/MyGame.java
+++ core/src/ru/larionov/MyGame.java
@@ -1,36 +1,19 @@
 package ru.larionov;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	TextureRegion background;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		background = new TextureRegion(new Texture("background_picture.jpg"), 0, 0, 550, 500);
-		img = new Texture("badlogic.jpg");
-	}
+import ru.larionov.screen.impl.MenuScreen;
+
+public class MyGame extends Game {
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
