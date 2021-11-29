cb9eb9b83cbe366ed431475d216fefa36a39f915
Larionov B <79999987388>
2021-11-29 17:31:00 +0300

    save

---
+++ core/src/ru/larionov/sprite/impl/Background.java
@@ -0,0 +1,21 @@
+package ru.larionov.sprite.impl;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.larionov.math.Rect;
+import ru.larionov.sprite.Sprite;
+
+public class Background extends Sprite {
+
+    public Background(Texture texture) {
+        super(new TextureRegion(texture));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(worldBounds.getHeight());
+        this.pos.set(worldBounds.pos);
+    }
+}
